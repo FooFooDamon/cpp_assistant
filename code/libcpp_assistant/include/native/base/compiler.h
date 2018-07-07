6de66aad4d3b880c38c81f3dc157001a79da224a
FooFooDamon <udc577@126.com>
2018-07-07 13:37:43 +0800

    Added CA_BEFORE_CPP_11 and CA_SINCE_CPP_11 macro

--- code/libcpp_assistant/include/native/base/compiler.h
+++ code/libcpp_assistant/include/native/base/compiler.h
@@ -73,7 +73,10 @@
 #define CA_PRINTF_CHECK(fmt_str_index, first_arg_index) \
     CA_FORMAT_CHECK(__printf__, fmt_str_index, first_arg_index)
 
-#if __cplusplus < 201103L
+#define CA_BEFORE_CPP_11							__cplusplus < 201103L
+#define CA_SINCE_CPP_11								__cplusplus >= 201103L
+
+#if CA_BEFORE_CPP_11
 #define override
 #define nullptr										NULL
 #endif // if __cplusplus < 201103L
