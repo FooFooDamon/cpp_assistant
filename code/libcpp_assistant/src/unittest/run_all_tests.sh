3b914fcf1f10c13bc5b0550f3722f5cc4091fc50
FooFooDamon <udc577@126.com>
2018-07-07 20:28:14 +0800

    Fixed the bug of searching for executable files on Ubuntu 18

--- code/libcpp_assistant/src/unittest/run_all_tests.sh
+++ code/libcpp_assistant/src/unittest/run_all_tests.sh
@@ -3,7 +3,7 @@ echo "Test directory: $TEST_DIR"
 export LD_LIBRARY_PATH=$TEST_DIR/../..
 cd $TEST_DIR
 rm -f failed_cases.txt
-for i in `file $TEST_DIR/* | grep executable | awk -F : '{ print $1 }'`
+for i in `file $TEST_DIR/* | grep -v "\.o\|\.cpp" | grep -i "executable\|ELF.*LSB" | awk -F : '{ print $1 }'`
 do
     echo "Running test case: $i ..."
     $i
