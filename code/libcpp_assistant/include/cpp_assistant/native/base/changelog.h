b29a708ebf3c224d2db5f6a8a33fd058215e4388
FooFooDamon <udc577@126.com>
2018-07-23 15:32:44 +0800

    Comments update

--- code/libcpp_assistant/include/cpp_assistant/native/base/changelog.h
+++ code/libcpp_assistant/include/cpp_assistant/native/base/changelog.h
@@ -51,10 +51,13 @@
  *
  * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
  *
- * wxc, 2018/07/22, 0.04.01:
+ * wxc, 2018/07/23, 0.04.01:
  *      1. Removed TLOG_XX(), GLOG_XX() and CA_XX(), and added LOGF(x)
  *          to do the same things instead.
- *      2. Replaced pthread_self() with gettid() within some APIs.
+ *      2. Defined LOGS(x), and fixed the newline bug of the first call
+ *          of logger::get_stream().
+ *      3. Replaced pthread_self() with gettid() within some APIs.
+ *      4. Added mutable version of logger::output_holder().
  *
  * wxc, 2018/07/21, 0.04.00:
  *      1. Added CA_LIB_NAMESPACE customization on compilation.
