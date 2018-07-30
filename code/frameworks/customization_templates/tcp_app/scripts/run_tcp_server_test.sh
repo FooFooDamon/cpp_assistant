5b8fc7acbce645aeae18f96eeca4ca5d52e6e362
FooFooDamon <udc577@126.com>
2018-07-30 22:36:06 +0800

    Absolute paths -> relative paths

--- code/frameworks/customization_templates/tcp_app/scripts/run_tcp_server_test.sh
+++ code/frameworks/customization_templates/tcp_app/scripts/run_tcp_server_test.sh
@@ -1,12 +1,13 @@
-mkdir ~/tmp/tcp_server_test/logs
-cd ~/tmp/tcp_server_test/logs || exit 1
+mkdir -p $(dirname $0)/../logs
+cd $(dirname $0)/.. || exit 1
 
 # SERVER_COUNT and CLIENT_COUNT depend on how many server/client items are configured in the relative XML file.
 # You may need to changes these if any configuration changes.
 SERVER_COUNT=3
 CLIENT_COUNT=3
 
-gnome-terminal -e 'bash -c "ulimit -c unlimited; sleep $(($RANDOM % $SERVER_COUNT)); ~/bin/tcp_server -c ~/etc/tcp_server.xml; read -p \"Press any key to continue ...\""' --tab --tab --tab
+#gnome-terminal -e 'bash -c "ulimit -c unlimited; sleep $(($RANDOM % $SERVER_COUNT)); ./tcp_server -c ./conf/servers.xml; read -p \"Press any key to continue ...\""' --tab --tab --tab
+gnome-terminal --tab --tab --tab -- sleep $(($RANDOM % $SERVER_COUNT)) && ./tcp_server -c ./conf/servers.xml && read -p "Press any key to continue ..."
 
-gnome-terminal -e 'bash -c "ulimit -c unlimited; sleep $(($RANDOM % $CLIENT_COUNT)); ~/bin/tcp_server -c ~/etc/tcp_client.xml; read -p \"Press any key to continue ...\""' --tab --tab --tab
-
+#gnome-terminal -e 'bash -c "ulimit -c unlimited; sleep $(($RANDOM % $CLIENT_COUNT)); ./tcp_server -c ./conf/clients.xml; read -p \"Press any key to continue ...\""' --tab --tab --tab
+gnome-terminal --tab --tab --tab -- sleep $(($RANDOM % $CLIENT_COUNT)) && ./tcp_server -c ./conf/clients.xml && read -p "Press any key to continue ..."
