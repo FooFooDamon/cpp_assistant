a79b4476fe3b224a6371026e4ff594cd72ff23f1
FooFooDamon <udc577@udc577>
2019-02-15 15:10:44 +0800

    Filtered .git directory while running update_code_copyright.sh

--- scripts/pack_cpp_assistant.sh
+++ scripts/pack_cpp_assistant.sh
@@ -5,12 +5,13 @@ version=`grep ".*, [0-9]\{4,\}[/-][0-9][0-9]\{1,2\}[/-][0-9]\{1,2\}, v.*" $SCRIP
 make clean -C $SCRIPT_DIR/../code/libcpp_assistant
 make clean -C $SCRIPT_DIR/../code/libcpp_assistant/src/unittest
 make update_copyright -C $SCRIPT_DIR/../code/libcpp_assistant
-for i in `find $SCRIPT_DIR/../code/frameworks/ -name "*.cpp" -o -name "*.h" | grep -v "\.svn"`
+for i in `find $SCRIPT_DIR/../code/frameworks/ -name "*.cpp" -o -name "*.h" | grep -v "\.svn" | grep -v "\.git"`
 do
 	update_code_copyright.sh $i
 done
 # tts.sh is a self-made script that turns all tabs in files into spaces.
 tts.sh $SCRIPT_DIR/../code
-tar -zcvf ~/cpp_assistant_${version}.tar.gz $SCRIPT_DIR/../../cpp_assistant/
+tar -zcvf ~/cpp_assistant_${version}.tar.gz $SCRIPT_DIR/../../cpp_assistant/ \
+	--exclude ".git" --exclude ".svn" --exclude "*.log" --exclude "*.a" --exclude "*.so" --exclude "*.so.*"
 nautilus ~/cpp_assistant_${version}.tar.gz
 
