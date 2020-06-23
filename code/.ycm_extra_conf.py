69becc69b6862d8f95845392c08c761fbc3e293d
FooFooDamon <udc577@126.com>
2020-06-23 10:26:09 +0800

    code/.ycm_extra_conf.py: + -I code/libcpp_assistant/src

--- code/.ycm_extra_conf.py
+++ code/.ycm_extra_conf.py
@@ -1,10 +1,12 @@
 import os, sys
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-INC_ROOT = os.path.join(PROJECT_ROOT, "code", "libcpp_assistant", "include")
+LIBCA_INC_ROOT = os.path.join(PROJECT_ROOT, "code", "libcpp_assistant", "include")
+LIBCA_SRC_ROOT = os.path.join(PROJECT_ROOT, "code", "libcpp_assistant", "src")
 sys.path.append(os.path.join(PROJECT_ROOT, "conf"))
 from ycm_basic_conf import *
-flags.extend([ "-I", INC_ROOT ])
-flags.extend([ "-I", os.path.join(INC_ROOT, "cpp_assistant", "native") ])
-flags.extend([ "-I", os.path.join(INC_ROOT, "cpp_assistant", "3rdparty") ])
-flags.extend([ "-I", os.path.join(INC_ROOT, "cpp_assistant", "3rdparty", "tinyxml") ])
+flags.extend([ "-I", LIBCA_INC_ROOT ])
+flags.extend([ "-I", os.path.join(LIBCA_INC_ROOT, "cpp_assistant", "native") ])
+flags.extend([ "-I", os.path.join(LIBCA_SRC_ROOT, "native") ])
+flags.extend([ "-I", os.path.join(LIBCA_INC_ROOT, "cpp_assistant", "3rdparty") ])
+flags.extend([ "-I", os.path.join(LIBCA_INC_ROOT, "cpp_assistant", "3rdparty", "tinyxml") ])
 
